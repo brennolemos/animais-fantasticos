ef39ec4493c82e4e4431879c4c8b598626416447
brennolemos <brenomiros@gmail.com>
2018-11-06 12:18:21 -0300

    Inicia Módulos

---
+++ js/modules/scroll-suave.js
@@ -0,0 +1,27 @@
+// Scroll Suave Link Interno
+export default function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+
+  function scrollToSection(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute('href');
+      const section = document.querySelector(href);
+      const topo = section.offsetTop;
+
+      section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+
+      // Forma Alternativa
+      // window.scrollTo({
+      //     top: topo,
+      //     behavior: 'smooth',
+      // });
+  }
+
+  linksInternos.forEach((link) => {
+      link.addEventListener('click', scrollToSection);
+  });
+
+}
